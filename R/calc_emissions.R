41621b06a2d7a2429edefe2ed2bad04523de643f
Darius Görgen <darius2402@web.de>
2022-03-01 11:19:05 +0100

    added greenhouse emission indicator

---
+++ R/calc_emissions.R
@@ -0,0 +1,126 @@
+#' Calculate tree cover per year based on GFW data sets
+#'
+#' Considering the 2000 GFW forest cover density layer users can specify
+#' a cover threshold above which a pixel is considered to be covered by forest.
+#' Additionally, users can specify a minimum size of a comprehensive patch of
+#' forest pixels. Patches below this threshold will not be considered as forest area.
+#'
+#' @param shp A single polygon for which to calculate the tree cover statistic
+#' @param treecover The treecover 2000 resource from GFW
+#' @param lossyear The lossyear resource from GFW
+#' @param minSize The minimum size of a forest patch in ha.
+#' @param minCover The minimum threshold of stand density for a pixel to be considered forest in the year 2000.
+#' @param rundir A directory where intermediate files are written to.
+#' @param verbose A directory where intermediate files are written to.
+#' @return A tibble
+#' @importFrom stringr str_sub
+#' @export
+#'
+.calc_emissions <- function(shp,
+                        treecover,
+                        lossyear,
+                        greenhouse,
+                        minSize = 10,
+                        minCover = 35,
+                        rundir = tempdir(),
+                        verbose = TRUE){
+
+  # initial argument checks
+  # retrieve years from portfolio
+  years = attributes(shp)$years
+  # handling of return value if resources are missing, e.g. no overlap
+  if(any(missing(treecover), missing(lossyear))){
+    return(as.data.frame(lapply(1:length(years), function(x) NA),
+                         col.names =  paste0("emissions_", years))
+    )
+  }
+  # check if treecover only contains 0s, e.g. on the ocean
+  if(all(length(unique(unlist(minmax(treecover)))) == 1, unique(unlist(minmax(treecover))) == 0)){
+    return(as.data.frame(lapply(1:length(years), function(x) 0),
+                         col.names =  paste0("emissions_", years))
+    )
+  }
+
+  # check additional arguments
+  minCover_msg = "Argument 'minCover' for indicator 'cover' must be a numeric value between 0 and 100."
+  if(is.numeric(minCover)){
+    minCover = as.integer(round(minCover))
+  } else {
+    stop(minCover_msg)
+  }
+  if(minCover < 0 || minCover > 100){
+    stop(minCover_msg)
+  }
+
+  minSize_msg = "Argument 'minSize' for indicator 'cover' must be a numeric value greater 0."
+  if(is.numeric(minSize)){
+    minSize = as.integer(round(minSize))
+  } else {
+    stop(minSize_msg)
+  }
+  if(minSize <= 0) stop(minSize_msg)
+
+  if(any(years < 2000)){
+    warning("Cannot calculate tree cover statistics for years smaller than 2000.")
+    years = years[years >= 2000]
+  }
+
+  #------------------------------------------------------------------------------
+  # start calculation if everything is set up correctly
+  # retrieve an area raster
+  arearaster = cellSize(treecover, unit = "ha",
+                        filename = file.path(rundir, "arearaster.tif"),
+                        datatype = "FLT4S",
+                        overwrite = TRUE)
+  # binarize the treecover layer based on minCover argument
+  binary_treecover = classify(treecover,
+                              rcl = matrix(c(0, minCover, 0, minCover, 100, 1), ncol = 3, byrow = TRUE),
+                              filename = file.path(rundir, "binary_treecover.tif"),
+                              datatype = "INT1U",
+                              overwrite = TRUE)
+  # retrieve patches of comprehensive forest areas
+  patched = patches(binary_treecover, directions = 8, zeroAsNA = TRUE,
+                    filename = file.path(rundir, "patched.tif"),
+                    datatype = "INT4U",
+                    overwrite = TRUE)
+  # get the sizes of the patches
+  patchsizes = zonal(arearaster, patched, sum, as.raster = TRUE,
+                     filename = file.path(rundir, "patchsizes.tif"),
+                     datatype = "FLT4S",
+                     overwrite = TRUE)
+  # remove patches smaller than threshold
+  binary_treecover = ifel(patchsizes < minSize, 0, binary_treecover,
+                          filename = file.path(rundir, "binary_treecover.tif"),
+                          datatype = "INT1U",
+                          overwrite = TRUE)
+  # set no loss occurrences to NA
+  lossyear = ifel(lossyear == 0, NA, lossyear,
+                  filename = file.path(rundir, "lossyear.tif"),
+                  datatype = "INT1U",
+                  overwrite = TRUE)
+  # rasterize the polygon
+  polyraster = rasterize(vect(shp), binary_treecover, field = 1,
+                         filename = file.path(rundir, "polygon.tif"),
+                         datatype = "INT1U",
+                         overwrite = TRUE)
+  # get forest cover statistics for each year
+  yearly_emission_values = lapply(years, function(y){
+    y = y - 2000
+    current_losslayer = ifel(lossyear == y , 1, NA,
+                             filename = file.path(rundir, "current_treelosses.tif"),
+                             datatype = "INT1U",
+                             overwrite = TRUE)
+    current_greenhouse = mask(greenhouse, current_losslayer,
+                              filename = file.path(rundir, "current_co2_emissions.tif"),
+                              datatype = "FLT4S",
+                              overwrite = TRUE)
+    # terra engine
+    emissions_sum = zonal(current_greenhouse, polyraster,  sum, na.rm = TRUE)[2]
+    as.numeric(emissions_sum)
+  })
+
+  # memory clean up
+  rm(arearaster, binary_treecover, patchsizes, patched, polyraster); gc()
+  # return a data-frame
+  as.data.frame(yearly_emission_values, col.names =  paste0("emissions_", years))
+}
--- R/resources_backlog.R
+++ R/resources_backlog.R
@@ -1,13 +1,13 @@
 #' Backlog function for available resources
 #'
-#' This function returns a list of either all available ressources and some
+#' This function returns a list of either all available resources and some
 #' additional metadata or for one or more requested resources. It can be used
 #' by users of the package to inform themselves about the available data sets
 #' and to learn about potentially additional arguments that should be specified
 #' when requesting the resource.
 #'
 #' @param resources Defaults to NULL meaning that a list with all available
-#'   resources will be returned. If a charachter vector is specified only the
+#'   resources will be returned. If a character vector is specified only the
 #'   information about the requested resource will be returned.
 #'
 #' @return A list object.
