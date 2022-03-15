fc28ebc8e18051514e7b04e3a3769a37c0c3e0cb
Ohm-Np <om.bhandari1995@gmail.com>
2022-03-15 10:33:21 +0000

    update indicator backlog with srtm

--- R/indicator_backlog.R
+++ R/indicator_backlog.R
@@ -1,28 +1,47 @@
-available_indicators <- function(indicator = NULL){
-  all_indicators = list(
+available_indicators <- function(indicator = NULL) {
+  all_indicators <- list(
     treecover = list(
       name = ".calc_cover",
-      inputs = list(treecover =
-                      "raster",
-                      lossyear = "raster"),
-      arguments = list(
-        minSize = 10,
-        minCover = 30)
-    ),
-    emissions = list(
-      name = ".calc_emissions",
-      inputs = list(treecover = "raster",
-                    lossyear = "raster",
-                    greenhouse = "raster"),
+      inputs = list(
+        treecover =
+          "raster",
+        lossyear = "raster"
+      ),
       arguments = list(
         minSize = 10,
         minCover = 30
+      )
+    ),
+    emissions = list(
+      name = ".calc_emissions",
+      inputs = list(
+        treecover = "raster",
+        lossyear = "raster",
+        greenhouse = "raster"
+      ),
+      arguments = list(
+        minSize = 10,
+        minCover = 30
+      )
+    ),
+    elevation = list(
+      name = ".calc_srtmElevation",
+      inputs = list(srtmelevation = "raster"),
+      arguments = list(
+        stats = "mean"
+      )
+    ),
+    tri <- list(
+      name = ".cal_TRI",
+      inputs = list(srtmelevation = "raster"),
+      arguments = list(
+        stats = c("mean", "median", "sd")
       )
     )
   )
 
   # determine what to return
-  if(is.null(indicator)) {
+  if (is.null(indicator)) {
     return(all_indicators)
   } else {
     .check_requested_indicator(indicator)
