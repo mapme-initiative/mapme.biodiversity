b6e603aae6fa34c9a488b3b688040c9dd108d735
fBedecarrats <f.bedecarrats@gmail.com>
2023-07-01 19:12:37 +0200

    launch minio locally on windows

--- trials/testing_s3.R
+++ trials/testing_s3.R
@@ -1,4 +1,9 @@
 devtools::install_github("cboettig/minioclient")
 
 library(minioclient)
+install_mc()
 test <- mc_ls("s3/fbedecarrats/mapme_biodiversity", recursive = TRUE)
+
+# Launch locally on Windows
+system(paste0(getwd(),
+              "/trials/minio.exe server C:/minio --console-address :9090"))
