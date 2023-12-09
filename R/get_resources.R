e7819125694d5108285fa27d3cd2faa41205d751
Darius A. Görgen <info@dariusgoergen.com>
2023-12-09 11:54:26 +0100

    improve precision roundtrip code

--- R/get_resources.R
+++ R/get_resources.R
@@ -121,15 +121,19 @@ get_resources <- function(x, resources, ...) {
 
 .make_footprints <- function(raster_files) {
   footprints <- lapply(unique(raster_files), function(file) {
+    # get BBOX and CRS
     tmp <- rast(file)
-    footprint <- tmp %>%
-      st_bbox() %>%
-      st_as_sfc(precision = 1e5) %>%
+    footprint <- st_bbox(tmp) %>% st_as_sfc()
+    crs <- crs(tmp)
+    # apply precision roundtrip
+    footprint <- st_sfc(footprint, precision = 1e5) %>%
       st_as_binary() %>%
-      st_as_sfc(crs = st_crs(tmp)) %>%
-      st_as_sf() %>%
-      dplyr::mutate(location = sources(tmp)) %>%
-      dplyr::rename(geom = "x")
+      st_as_sfc()
+    # to sf and add location info
+    footprint %>%
+      st_as_sf(crs = crs) %>%
+      dplyr::rename(geom = "x") %>%
+      dplyr::mutate(location = file)
   })
   do.call(rbind, footprints)
 }
