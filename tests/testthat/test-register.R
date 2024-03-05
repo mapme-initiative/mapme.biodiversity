7ca8a5404c58dd63cc29347c0b35a343139c93c7
Darius A. Görgen <info@dariusgoergen.com>
2024-03-05 15:47:36 +0100

    Update backend tests

--- tests/testthat/test-register.R
+++ tests/testthat/test-register.R
@@ -1,116 +1,118 @@
+test_that("mapme_options works", {
+  opts <- mapme_options()
+  expect_equal(names(opts), c("outdir", "tempdir", "verbose", "aria_bin", "testing"))
+
+  expect_error(mapme_options(outdir = 1))
+  expect_error(mapme_options(tempdir = 1))
+  expect_error(mapme_options(tempdir = "/does-not-exists"))
+  expect_error(mapme_options(verbose = 1))
+  expect_error(mapme_options(testing = 1))
+})
+
 test_that("test register_resource works", {
   name <- "sample"
   type <- "raster"
   source <- "sample_source"
-  test_fun <- function() {}
-  arguments <- list(
-    a = 1,
-    b = 2
-  )
-
 
   expect_error(
     register_resource(),
-    "neither name, type, fun or arguments can be NULL"
+    "neither name, type, or source can be NULL"
   )
 
   expect_error(
     register_resource(
-      name = 1, type = type, source = source,
-      fun = test_fun, arguments = arguments
+      name = 1, type = type, source = source
     ),
     "name needs to be a single charachter string"
   )
 
   expect_error(
     register_resource(
-      name = name, type = "unknown", source = source,
-      fun = test_fun, arguments = arguments
+      name = name, type = "unknown", source = source
     ),
     "type needs to be one of 'vector' or 'raster'"
   )
 
   expect_error(
     register_resource(
-      name = name, type = type, source = 1,
-      fun = test_fun, arguments = arguments
+      name = name, type = type, source = 1
     ),
     "source needs to be a single charachter string"
   )
 
-  expect_error(
-    register_resource(
-      name = name, type = type, source = source,
-      fun = "test_fun", arguments = arguments
-    ),
-    "fun needs to be a valid function signature"
-  )
-
-  expect_error(
-    register_resource(
-      name = name, type = type, source = source,
-      fun = test_fun, arguments = "arguments"
-    ),
-    "arguments needs to be a list."
-  )
+  expect_silent(register_resource(name, type, source))
+  res <- .pkgenv$resources[name]
+  expect_equal(names(res), name)
+  res <- res[[name]]
+  expect_equal(names(res), c("type", "source"))
+  expect_equal(res[["type"]], type)
+  expect_equal(res[["source"]], source)
 })
-
-
 
 test_that("test register_indicator works", {
   name <- "sample"
   resources <- list(gfw_treecover = "raster", gmw = "vector")
-  processing_mode <- "asset"
-  test_fun <- function() {}
-  arguments <- list(
-    a = 1,
-    b = 2
-  )
-
 
   expect_error(
     register_indicator(),
-    "neither name, resources, fun, arguments, or processing_mode can be NULL"
+    "neither name nor resources can be NULL"
   )
 
   expect_error(
     register_indicator(
-      name = 1, resources = resources, fun = test_fun,
-      processing_mode = processing_mode, arguments = arguments
+      name = 1, resources = resources
     ),
     "name needs to be a single charachter string"
   )
 
   expect_error(
     register_indicator(
-      name = name, fun = test_fun, arguments = arguments,
-      processing_mode = processing_mode,
+      name = name,
       resources = append(resources, list(gfw_lossyear = "unknown"))
     ),
     "the following resources have an unknown type specified"
   )
 
-  expect_error(
-    register_indicator(
-      name = name, resources = resources, fun = 1,
-      processing_mode = processing_mode, arguments = arguments
-    ),
-    "fun needs to be a valid function signature"
-  )
+  expect_silent(register_indicator(name, resources))
+  ind <- .pkgenv$indicators[name]
+  expect_equal(names(ind), name)
+  ind <- ind[[name]]
+  expect_equal(names(ind), "resources")
+  ind <- ind[["resources"]]
+  expect_equal(names(ind), c("gfw_treecover", "gmw"))
+  expect_equal(ind[["gfw_treecover"]], "raster")
+  expect_equal(ind[["gmw"]], "vector")
+})
 
-  expect_error(
-    register_indicator(
-      name = name, resources = resources, fun = test_fun,
-      processing_mode = "unknown", arguments = arguments
-    ),
-    "processing_mode needs to be one of 'asset' or 'portfolio'"
-  )
+test_that("test available_resources works", {
+  res <- available_resources()
+  expect_equal(class(res), "list")
+  expect_true(length(res) > 1)
+  expect_error(available_resources("not-available"))
+})
 
-  expect_error(
-    register_indicator(
-      name = name, resources = resources, fun = test_fun,
-      processing_mode = processing_mode, arguments = 1
-    ),
-    "arguments needs to be a list."
-  )
+test_that("test available_indicators works", {
+  ind <- available_indicators()
+  expect_equal(class(ind), "list")
+  expect_true(length(ind) > 1)
+  expect_error(available_indicators("not-available"))
+})
+
+
+test_that("add / clear resource works", {
+  .clear_resources()
+  add <- list(1:10)
+  names(add) <- "test_res"
+  res <- .avail_resources()
+  expect_true(class(res) == "list")
+  expect_length(res, 0)
+  .add_resource(add)
+  res <- .avail_resources()
+  expect_true(class(res) == "list")
+  expect_length(res, 1)
+  expect_equal(names(res), "test_res")
+  expect_equal(res[["test_res"]], 1:10)
+  .clear_resources()
+  res <- .avail_resources()
+  expect_length(res, 0)
 })
