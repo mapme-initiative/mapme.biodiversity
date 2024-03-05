887274b4fe7f3012beac038423cadd96423e6ad8
Darius A. Görgen <info@dariusgoergen.com>
2024-03-05 15:39:58 +0100

    Update worldop to closure UI

--- tests/testthat/test-calc_population_count.R
+++ tests/testthat/test-calc_population_count.R
@@ -8,21 +8,17 @@ test_that(".calc_population_count works", {
     package = "mapme.biodiversity"
   ), pattern = ".tif$", full.names = TRUE)
   worldpop <- rast(worldpop)
-  attributes(shp)$years <- 2000:2022
-  expect_error(
-    .calc_population_count(shp, worldpop, engine = "not-available"),
-    "Engine 'not-available' is not an available engine. Please choose one of:"
-  )
-  expect_error(
-    .calc_population_count(shp, worldpop, stats_popcount = "not-available"),
-    "Statistic 'not-available' is not supported. Please choose one of:"
-  )
 
-  result <- .calc_population_count(shp, worldpop)
-  result_multi_stat <- .calc_population_count(shp, worldpop, stats_popcount = c("mean", "median", "sd"))
-  result_zonal <- .calc_population_count(shp, worldpop, engine = "zonal")
-  result_extract <- .calc_population_count(shp, worldpop, engine = "extract")
-  result_exact <- .calc_population_count(shp, worldpop, engine = "exactextract")
+  cpc <- calc_popcount()
+  result <- cpc(shp, worldpop)
+  cpc <- calc_popcount(stats = c("mean", "median", "sd"))
+  result_multi_stat <- cpc(shp, worldpop)
+  cpc <- calc_popcount(engine = "zonal")
+  result_zonal <- cpc(shp, worldpop)
+  cpc <- calc_popcount(engine = "extract")
+  result_extract <- cpc(shp, worldpop)
+  cpc <- calc_popcount(engine = "exactextract")
+  result_exact <- cpc(shp, worldpop)
 
   expect_equal(
     names(result),
