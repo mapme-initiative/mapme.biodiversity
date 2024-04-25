b5db8acb81fb071017b414e64ebe2a12f2616fb0
Darius A. Görgen <info@dariusgoergen.com>
2024-04-25 15:46:42 +0200

    update mangroves test

--- tests/testthat/test-calc_mangroves_area.R
+++ tests/testthat/test-calc_mangroves_area.R
@@ -16,7 +16,7 @@ test_that("mangrove extent works", {
   ma <- calc_mangroves_area()
   expect_true(is.null(ma(x, NULL)))
   result <- ma(x, gmw)
-  expect_equal(unique(result$variable), "mangrove_area")
+  expect_equal(unique(result$variable), "mangroves")
   expect_equal(result$value, c(1214.88, 1206.61), tolerance = 1e-4)
   expect_equal(format(result$datetime, "%Y"), c("1996", "2016"))
 })
