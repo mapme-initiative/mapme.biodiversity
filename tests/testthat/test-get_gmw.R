f0e05e3f956902ca7b618f4aebd07755cc5de03d
Darius A. Görgen <info@dariusgoergen.com>
2024-05-10 13:11:31 +0200

    Adapts get_gmw to footprint backend

--- tests/testthat/test-get_gmw.R
+++ tests/testthat/test-get_gmw.R
@@ -1,25 +1,10 @@
 test_that(".get_gmw works", {
   skip_on_cran()
-  aoi <- read_sf(
-    system.file("extdata", "shell_beach_protected_area_41057_B.gpkg",
-      package = "mapme.biodiversity"
-    )
-  )
-  aoi <- suppressWarnings(st_cast(aoi, to = "POLYGON")[1, ])
 
-  temp_loc <- file.path(tempdir(), "mapme.biodiversity")
-  dir.create(temp_loc, showWarnings = FALSE)
-  resource_dir <- system.file("res", package = "mapme.biodiversity")
-  file.copy(resource_dir, temp_loc, recursive = TRUE)
-  outdir <- file.path(tempdir(), "mapme.biodiversity", "res")
-  tmpdir <- tempdir()
-
-  mapme_options(
-    outdir = outdir,
-    tmpdir = tmpdir,
-    verbose = FALSE,
-    testing = TRUE
-  )
-  gmw <- get_gmw(years = 2007:2008)
-  expect_equal(gmw(), c("gmw-extent_2007.zip", "gmw-extent_2008.zip"))
+  expect_error(get_gmw(2011))
+  expect_silent(gmw <- get_gmw(years = 2007:2008))
+  expect_silent(.check_resource_fun(gmw))
+  expect_silent(fps <- gmw())
+  expect_silent(.check_footprints(fps))
+  expect_equal(fps$filename, c("gmw_v3_2007_vec.gpkg", "gmw_v3_2008_vec.gpkg"))
 })
