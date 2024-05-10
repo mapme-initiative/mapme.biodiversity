4f53833023e9f20f27685ddb802e88fee6b5b0e1
Darius A. Görgen <info@dariusgoergen.com>
2024-05-10 13:08:14 +0200

    Adapts get_chirps to footprint backend

--- tests/testthat/test-get_chirps.R
+++ tests/testthat/test-get_chirps.R
@@ -1,24 +1,10 @@
 test_that(".get_chirps works", {
   skip_on_cran()
-  aoi <- read_sf(
-    system.file("extdata", "sierra_de_neiba_478140.gpkg",
-      package = "mapme.biodiversity"
-    )
-  )
-  aoi <- suppressWarnings(st_cast(aoi, to = "POLYGON")[1, ])
 
-  outdir <- file.path(tempdir(), "mapme-data")
-  .copy_resource_dir(outdir)
-
-  mapme_options(
-    outdir = outdir,
-    verbose = FALSE,
-    testing = TRUE
-  )
-
+  expect_error(get_chirps(1970))
   gc <- get_chirps(years = 2010)
-  urls <- gc(aoi)
-  exts <- unique(substr(urls, nchar(urls[1]) - 5, nchar(urls[1])))
-  expect_equal(exts, "tif.gz")
-  expect_gte(length(urls), 12)
+  expect_silent(.check_resource_fun(gc))
+  fps <- gc(aoi)
+  expect_silent(fps <- .check_footprints(fps))
+  expect_equal(nrow(fps), 12)
 })
