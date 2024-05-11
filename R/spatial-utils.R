ad7ae21e40536454948372ae92dce1e2fb7bd4e0
Darius A. Görgen <info@dariusgoergen.com>
2024-05-11 10:40:08 +0200

    Re-organize spatial utils

--- R/spatial-utils.R
+++ R/spatial-utils.R
@@ -1,99 +1,136 @@
-.vector_footprint <- function(src, opts = NULL) {
-  info <- sf::gdal_utils("ogrinfo", src, options = c("-json", "-so", "-ro", "-nomd", "-nocount", opts), quiet = TRUE)
+#### -------------------------Vector footprint------------------------------####
+.vector_footprint <- function(src, oo = NULL) {
+  layers_info <- .vector_info(src, oo)
+  if (is.null(layers_info)) {
+    return(NULL)
+  }
+
+  bboxs <- purrr::map_vec(layers_info, .vector_bbox)
+  bbox <- st_as_sf(st_union(bboxs))
+  st_geometry(bbox) <- "geometry"
+  bbox["source"] <- src
+  bbox
+}
+
+.vector_info <- function(src, oo) {
+  stopifnot(is.character(src) && length(src) == 1)
+  stopifnot(is.null(oo) || is.character(oo))
+
+  info <- sf::gdal_utils(
+    "ogrinfo",
+    source = src,
+    options = c("-json", "-so", "-ro", "-nomd", "-nocount", oo),
+    quiet = TRUE
+  )
+
   if (length(info) == 0) {
     return(NULL)
   }
+
   info <- jsonlite::parse_json(info)
-  layers <- info[["layers"]]
-
-  layers_bbox <- purrr::map_dfr(layers, function(layer) {
-    crs <- layer[["geometryFields"]][[1]][["coordinateSystem"]][["wkt"]] %>% st_crs()
-    bbox <- layer[["geometryFields"]][[1]][["extent"]] %>% as.numeric()
-    class(bbox) <- "bbox"
-
-    bbox <- st_as_sf(st_as_sfc(bbox))
-    st_crs(bbox) <- crs
-    bbox
-  })
-
-  crs <- st_crs(layers_bbox)
-  bbox <- st_union(layers_bbox) %>%
-    st_as_sf() %>%
-    dplyr::rename(geometry = "x") %>%
-    dplyr::mutate(source = src)
-
-  st_crs(bbox) <- crs
-  bbox[, "source"]
+  info[["layers"]]
 }
 
-.raster_footprint <- function(src, opts = NULL) {
-  info <- sf::gdal_utils("gdalinfo", src, options = c("-json", "-norat", "-noct", "-nomd", opts), quiet = TRUE)
-  info <- jsonlite::parse_json(info)
-  crs <- st_crs(info[["coordinateSystem"]][["wkt"]])
+.vector_bbox <- function(layer) {
+  crs <- layer[["geometryFields"]][[1]][["coordinateSystem"]][["wkt"]]
+  crs <- st_crs(crs)
+  bbox <- as.numeric(layer[["geometryFields"]][[1]][["extent"]])
+  names(bbox) <- c("xmin", "ymin", "xmax", "ymax")
+  st_as_sfc(st_bbox(bbox, crs = crs))
+}
 
-  bbox <- info[["wgs84Extent"]] %>%
-    jsonlite::toJSON(auto_unbox = TRUE) %>%
-    read_sf() %>%
-    dplyr::mutate(source = src)
-  st_crs(bbox) <- st_crs(4326)
+#### -------------------------Raster footprint------------------------------####
+.raster_footprint <- function(src, oo = NULL) {
+  info <- .raster_info(src, oo)
+  if (is.null(info)) {
+    return(NULL)
+  }
+  .raster_bbox(info)
+}
+
+.raster_info <- function(src, oo = NULL) {
+  stopifnot(is.character(src) && length(src) == 1)
+  stopifnot(is.null(oo) || is.character(oo))
+
+  info <- sf::gdal_utils(
+    "gdalinfo",
+    source = src,
+    options = c("-json", "-norat", "-noct", "-nomd", oo),
+    quiet = TRUE
+  )
+
+  if (length(info) == 0) {
+    return(NULL)
+  }
+
+  jsonlite::parse_json(info)
+}
+
+.raster_bbox <- function(info) {
+  crs <- st_crs(info[["coordinateSystem"]][["wkt"]])
+  poly <- jsonlite::toJSON(info[["wgs84Extent"]], auto_unbox = TRUE)
+  bbox <- st_read(poly, quiet = TRUE)
   bbox <- st_transform(bbox, crs)
 
   if (st_is_empty(bbox)) {
-    coords <- info$cornerCoordinates
+    coords <- info[["cornerCoordinates"]]
     bbox <- st_bbox(c(
       xmin = coords$lowerLeft[[1]],
       xmax = coords$upperRight[[1]],
       ymin = coords$lowerLeft[[2]],
       ymax = coords$upperLeft[[2]]
-    ), crs = crs) %>%
-      st_as_sfc() %>%
-      st_as_sf() %>%
-      dplyr::mutate(source = src)
+    ), crs = crs)
+    bbox <- st_as_sf(st_as_sfc(bbox))
   }
-  bbox[, "source"]
+
+  bbox["source"] <- src
+  bbox
 }
 
+#### -------------------------Unexported utils------------------------------####
 .set_precision <- function(data, precision = 1e5) {
   crs <- st_crs(data)
-  geoms <- st_geometry(data) %>%
-    st_sfc(precision = precision) %>%
-    st_as_binary() %>%
-    st_as_sfc()
+  geoms <- st_geometry(data)
+  geoms <- st_sfc(geoms, precision = precision)
+  geoms_binary <- st_as_binary(geoms)
+  geoms <- st_as_sfc(geoms_binary)
   st_geometry(data) <- geoms
   st_geometry(data) <- "geometry"
   st_crs(data) <- crs
-  data
+  .geom_last(data)
 }
 
 
-.get_spds <- function(src, dest, oo = character(0), co = character(0),
-                      what = c("vector", "raster"),
-                      gdal_conf = NULL) {
+.get_spds <- function(source = NULL,
+                      destination = NULL,
+                      opts = NULL,
+                      what = c("vector", "raster")) {
   what <- match.arg(what)
-  withr::with_envvar(gdal_conf, code = if (spds_exists(dest)) {
+  stopifnot(is.character(source) && length(source) == 1)
+  stopifnot(is.character(destination) && length(destination) == 1)
+  stopifnot(is.null(opts) || is.character(opts))
+  if (is.null(opts)) opts <- character(0)
+
+  does_exist <- spds_exists(destination)
+  if (does_exist) {
     return(TRUE)
-  })
-  if (is.null(oo)) oo <- character(0)
-  if (is.null(co)) co <- character(0)
-  opts <- c(oo, co)
+  }
 
   util <- switch(what,
     vector = "vectortranslate",
     raster = "translate"
   )
-  withr::with_envvar(gdal_conf, code = {
-    if (spds_exists(dest)) {
-      return(TRUE)
-    }
-    try(sf::gdal_utils(
-      util = util,
-      source = src,
-      destination = dest,
-      options = opts
-    ))
-  })
-  withr::with_envvar(gdal_conf, code = return(spds_exists(dest)))
+  try(sf::gdal_utils(
+    util = util,
+    source = source,
+    destination = destination,
+    options = opts
+  ))
+
+  return(spds_exists(destination))
 }
+
+#### -------------------------Exported utils------------------------------####
 
 #' Check if a spatial data sets exists
 #'
@@ -123,31 +160,27 @@
 #' # a non existing file
 #' spds_exists("not-here.gpkg", what = "vector")
 #'
-spds_exists <- function(path,
-                        what = c("vector", "raster")) {
+spds_exists <- function(path, oo = NULL, what = c("vector", "raster")) {
   what <- match.arg(what)
-
-  info <- switch(what,
-    vector = sf::gdal_utils("ogrinfo", path,
-      options = c(
-        "-json", "-ro", "-so", "-nomd",
-        "-nocount", "-noextent", "-nogeomtype"
-      ),
-      quiet = TRUE
-    ),
-    raster = sf::gdal_utils("gdalinfo", path,
-      options = c("-json", "-nomd", "-norat", "-noct"),
-      quiet = TRUE
-    )
+  util <- switch(what,
+    vector = "ogrinfo",
+    raster = "gdalinfo"
   )
-
-  if (length(info) == 0) {
-    return(FALSE)
-  } else {
-    return(TRUE)
-  }
+  opts <- switch(what,
+    vector = c(
+      "-json", "-ro", "-so", "-nomd",
+      "-nocount", "-noextent", "-nogeomtype", oo
+    ),
+    raster = c("-json", "-nomd", "-norat", "-noct", oo)
+  )
+  info <- sf::gdal_utils(
+    util = util,
+    source = path,
+    options = opts,
+    quiet = TRUE
+  )
+  length(info) > 0
 }
-
 
 #' Create footprints for vector or raster data sets
 #'
@@ -164,7 +197,7 @@ spds_exists <- function(path,
 #' @param srcs A character vector with GDAL readable paths to either vector
 #'  or raster sources, then internal footprint functions are called, or an
 #'  sf object which will be appended for filenames and potential options.
-#' @param filenames A charcter vector indicating the filenames of the source
+#' @param filenames A character vector indicating the filenames of the source
 #'   data sets if they were written to a destionation. Defaults to `basename(srcs)`
 #'   in case of character type or `basename(srcs[["source"]])` in case of
 #'   an sf object.
@@ -219,8 +252,8 @@ make_footprints <- function(srcs = NULL,
   if (inherits(srcs, "character")) {
     what <- match.arg(what)
     srcs <- switch(what,
-      vector = purrr::map2(srcs, oo, function(src, oo) .vector_footprint(src, oo)),
-      raster = purrr::map2(srcs, oo, function(src, oo) .raster_footprint(src, oo)),
+      vector = purrr::map2(srcs, oo, function(src, opt) .vector_footprint(src, opt)),
+      raster = purrr::map2(srcs, oo, function(src, opt) .raster_footprint(src, opt)),
       stop("Can make footprints for vector and raster data only.")
     )
     srcs <- purrr::list_rbind(srcs)
