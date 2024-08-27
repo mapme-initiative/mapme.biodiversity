814bb56063c62c933fe2b217fe278da00bb996b3
Darius A. Goergen <info@dariusgoergen.com>
2024-08-27 08:40:35 +0000

    update bii

--- R/get_biodiversity_intactness_index.R
+++ R/get_biodiversity_intactness_index.R
@@ -17,30 +17,30 @@
 #'   (2016). Global map of the Biodiversity Intactness Index, from Newbold et al.
 #'   (2016) Science \[Data set\]. Natural History Museum.
 #'   \doi{https://doi.org/10.5519/0009936}
-#' @source \url{https://data.nhm.ac.uk/dataset/global-map-of-the-biodiversity-intactness-index-from-newbold-et-al-2016-science}
+#' @source \url{https://data.nhm.ac.uk/dataset/}
 #' @include register.R
 #' @export
 get_biodiversity_intactness_index <- function(path = NULL) {
-
-  if(is.null(path) || !file.exists(path)) {
+  if (is.null(path) || !file.exists(path)) {
     stop("Expecting path to point towards an existing file.")
   }
-  if(!endsWith(path, ".asc")) {
+  if (!endsWith(path, ".asc")) {
     stop("Unexpected file extension: path must point towards a '.asc' file.")
   }
 
   function(
-    x,
-    name = "biodiversity_intactness_index",
-    type = "raster",
-    outdir = mapme_options()[["outdir"]],
-    verbose = mapme_options()[["verbose"]]) {
+      x,
+      name = "biodiversity_intactness_index",
+      type = "raster",
+      outdir = mapme_options()[["outdir"]],
+      verbose = mapme_options()[["verbose"]]) {
     filename <- gsub("asc", "tif", basename(path))
     fps <- make_footprints(
       path,
       filename,
       what = "raster",
-      co = c("-co", "INTERLEAVE=BAND", "-co", "COMPRESS=LZW", "-ot", "Float32"))
+      co = c("-co", "INTERLEAVE=BAND", "-co", "COMPRESS=LZW", "-ot", "Float32")
+    )
     st_crs(fps) <- st_crs("EPSG:4326")
     fps
   }
